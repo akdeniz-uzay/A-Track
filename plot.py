12b5fd8a37bf95d98f66b3763ac2ab50b56038ad
yucelkilic <yucelkilic1@gmail.com>
2015-03-01 22:04:26 +0200

    Line detect function expanded with plot function.

--- plot.py
+++ plot.py
@@ -13,15 +13,15 @@ import glob
 class Plot:
     """
     MOD's plotting class.
-        		
+                
     """
     
     def catalogue(self, star_cat, target_folder):
         """
         Reads given ordered (corrected) coordinate file plot stars.
-        :param star_cat: Ordered (corrected) star catalogue.
-        :type star_cat: Text file object..
-            		
+        @param star_cat: Ordered (corrected) star catalogue.
+        @type star_cat: Text file object..
+                    
         """
         catxy = pd.read_csv(star_cat, sep=",", names=["ref_x", "ref_y"], header=0)
         if not catxy.empty:
@@ -42,27 +42,29 @@ class Plot:
             plt.close()
         else:
             print "No objects detected on %s." %(starcatfile)
+        return
 
 
     def plotallcat(self, ordered_cats, target_folder):
         """
         Reads and plots all ordered (corrected) coordinate files under given directory.
         
-        :param ordered_cats: Ordered coordinate folder.
-        :type stray_cats: Directory object.
-            		
+        @param ordered_cats: Ordered coordinate folder.
+        @type stray_cats: Directory object.
+                    
         """      
         for catfile in sorted(glob.glob("%s/*affineremap.txt" %(ordered_cats))):
             self.catalogue(catfile, target_folder)
-            print "Ploted all detected objects on: %s." %(os.path.basename(catfile))        
+            print "Ploted all detected objects on: %s." %(os.path.basename(catfile))
+        return
 
     def plotallobjects(self, ordered_cats, target_folder):
         """
         Reads all catalogue files under given ordered catalogue folder to draw all objects in one figure.
         
-        :param ordered_cats: Ordered coordinate folder.
-        :type stray_cats: Directory object.
-            		
+        @param ordered_cats: Ordered coordinate folder.
+        @type stray_cats: Directory object.
+                    
         """      
         starcat = sorted(glob.glob("%s/*affineremap.txt" %(ordered_cats)))
         onecatlist = pd.DataFrame(columns=["ref_x", "ref_y"])
@@ -82,17 +84,57 @@ class Plot:
         ax.set_aspect('equal', 'datalim')    
         plt.savefig("%s/allobjects.png" %(target_folder))
         plt.close()
+        return
 
     def plot(self, coorlist, output_figure = None):
+        """
+        Plots data (x, y) object
+        @param coorlist: Array data (x, y).
+        @type coorlist: Array
+        @param output_figure: PNG file.
+        @type output_figure: Directory or file object.
+        """
         coorlist.plot(kind="scatter", x = "ref_x", y = "ref_y", xlim=(0, 2048), ylim=(0, 2048), fontsize=12, figsize=(10, 10))
         ax = plt.gca()
         ax.set_aspect('equal', 'datalim')
-        plt.title("Detected %s objects." % (len(coorlist)))
-        if output_figure != "-nosave":
+        plt.title("Detected %s objects." %(len(coorlist)))
+        if output_figure != "-nosave" and output_figure != "-noplot":
             plt.savefig("%s" %(output_figure))
             plt.close()
         elif output_figure == "-nosave":
             plt.show()
             plt.close()
+        elif output_figure == "-noplot":
+            pass
+        return
+
+    def fits2png(self, alifilepath, outdir, datalxy = None):
+        """
+        f2n is a tiny python module to transfrom FITS images into PNG files, built around pyfits and PIL. I tend to include these PNG files into html pages for fast visualization, that's why the module is called "fits to net".
+        More information: http://obswww.unige.ch/~tewes/f2n_dot_py
+
+        @param alifilepath: FITS file.
+        @type alifilepath: File object.
+        @param outdir: PNG file out directory.
+        @type outdir: Directory object.
+        """
+        try:
+            import f2n
+        except ImportError:
+            print "Couldn't import f2n -- install it !"
+            return
+        myimage = f2n.fromfits(alifilepath, verbose=False)
+        myimage.setzscale("auto", "auto")
+        myimage.makepilimage("log", negative = False)
+
+        if not datalxy.empty:
+            for i in xrange(len(datalxy)):
+                myimage.drawrectangle(datalxy.values[i][1] - 10, datalxy.values[i][1] + 10, datalxy.values[i][2] - 10, datalxy.values[i][2] + 10, colour=(0,255,0), label="%s" %(i))
+        myimage.writetitle(os.path.basename(alifilepath))
+        if not os.path.isdir(outdir):
+                os.makedirs(outdir)
+        base_alifilepath = os.path.basename(alifilepath)
+        h_alifilepath, e_alifilepath = base_alifilepath.split(".")
+        myimage.tonet(os.path.join(outdir, h_alifilepath + ".png"))
         
         
