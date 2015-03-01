55b3dd6043659665b947263be6319f181181c107
yucelkilic <yucelkilic1@gmail.com>
2015-03-01 22:28:52 +0200

    Empty lines removed.

--- asterotrek.py
+++ asterotrek.py
@@ -108,15 +108,10 @@ def astromods(ordered_cats, target_folder):
 def cleancosmics(image, odir, gain=2.2, readnoise=10.0, sigclip = 5.0, sigfrac = 0.3, objlim = 5.0, maxiter = 4):
     """
     Detect and clean cosmic ray hits on images (numpy arrays or FITS), using scipy, and based on Pieter van Dokkum's L.A.Cosmic algorithm.
-    
     @sigclip: increase this if you detect cosmics where there are none. Default is 5.0, a good value for earth-bound images. objlim : increase this if normal stars are detected as cosmics. Default is 5.0, a good value for earth-bound images.
-    
     Constructor of the cosmic class, takes a 2D numpy array of your image as main argument. sigclip : laplacian-to-noise limit for cosmic ray detection objlim : minimum contrast between laplacian image and fine structure image. Use 5.0 if your image is undersampled, HST, ...
-    
     @satlevel: if we find agglomerations of pixels above this level, we consider it to be a saturated star and do not try to correct and pixels around it. A negative satlevel skips this feature.
-    
-    pssl is the previously subtracted sky level !
-    
+    pssl is the previously subtracted sky level!
     @real gain = 1.8 # gain (electrons/ADU) (0=unknown) real readn = 6.5 # read noise (electrons) (0=unknown) ##gain0 string statsec = "*,*" # section to use for automatic computation of gain real skyval = 0. # sky level that has been subtracted (ADU) real sigclip = 3.0 # detection limit for cosmic rays (sigma) real sigfrac = 0.5 # fractional detection limit for neighbouring pixels real objlim = 3.0 # contrast limit between CR and underlying object int niter = 1 # maximum number of iterations
     More information: http://obswww.unige.ch/~tewes/cosmics_dot_py/    
     """
