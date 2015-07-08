3813aa19e90ebb0f2c1e54cfd7193591330c71d5
Yücel Kılıç <yucelkilic1@gmail.com>
2015-07-08 03:04:29 +0300

    ADD: update pyds9

--- plot.py
+++ plot.py
@@ -7,7 +7,7 @@ Created on Sat Feb 21 18:33:56 2015
 
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import os, signal
 import glob
 import pyfits
 
@@ -38,7 +38,7 @@ class Plot:
         try:
             if datalxy.size:
                 for i in xrange(len(datalxy)):
-                    myimage.drawrectangle(datalxy[i][2] - 10, datalxy[i][2] + 10, datalxy[i][3] - 10, datalxy[i][3] + 10, colour=(0,255,0), label="%s" %(int(datalxy[i][0])))
+                    myimage.drawrectangle(datalxy[i][2] - 10, datalxy[i][2] + 10, datalxy[i][3] - 10, datalxy[i][3] + 10, colour=(0,255,0), label="%s" %(int(datalxy[i][6])))
         except AttributeError:
             pass
         myimage.writetitle(os.path.basename(alifilepath))
@@ -82,42 +82,42 @@ class Plot:
         """
         import os
         try:
-            import ds9 as ds
+            import pyds9 as ds
         except:
-        	print("pyds9'? http://hea-www.harvard.edu/RD/pyds9/")
-        	raise SystemExit  
+            print("pyds9'? http://hea-www.harvard.edu/RD/pyds9/")
+            raise SystemExit
 
         print '\033[1;32mNow, openning DS9!\033[0m'
-        try: 
-            os.popen("kill -9  $(pidof ds9)")
-            d = ds.ds9()
+        try:
+            d = ds.DS9()
             d.set("file %s" %(fits_path))
             #Zoom to fit
             d.set('zoom to fit')
             #Scale to zscales
             d.set('scale zscale')
-            #Getting Params 
+            #Getting Params
+
+            catfile = os.path.basename(cat_path)
+            h_catfile, e_catfile = catfile.split(".")
+            
+            if e_catfile == "pysexcat":
+                print '\033[1;34mDetecting stars on %s!\033[0m' %(cat_path)
+                coordinate = os.popen("cat %s|grep -v '#'|awk '{print $2,$3}'" %(cat_path))
+            elif e_catfile == "txt":
+                print '\033[1;34mDetecting stars on %s!\033[0m' %(cat_path)
+                coordinate = os.popen("cat %s|tail -n +2|awk -F' *, *' '{print $2,$3}'" %(cat_path))
+            elif e_catfile == "cat":
+                print '\033[1;34mDetecting stars on %s!\033[0m' %(cat_path)
+                coordinate = os.popen("cat %s|tail -n +2|awk '{print $2,$3}'" %(cat_path)) 
+            counter = 0
+            for line in coordinate.readlines():
+                counter += 1
+                line = line.replace("\n", "")
+                x_coor,  y_coor = line.split(" ")
+                d.set('regions', 'image; circle(%s,%s,5) # color=red text={%s}' %(x_coor,  y_coor, counter))
+            print '\033[1;34mDetecting stars job is completed!\033[0m'
         except:
             print '\033[1;31mSomething went wrong with DS9!\033[0m'
-        
-        catfile = os.path.basename(cat_path)
-        h_catfile, e_catfile = catfile.split(".")
-        
-        if e_catfile == "pysexcat":
-            print '\033[1;34mDetecting stars on %s!\033[0m' %(cat_path)
-            coordinate = os.popen("cat %s|grep -v '#'|awk '{print $2,$3}'" %(cat_path))
-        elif e_catfile == "txt":
-            print '\033[1;34mDetecting stars on %s!\033[0m' %(cat_path)
-            coordinate = os.popen("cat %s|tail -n +2|awk -F' *, *' '{print $2,$3}'" %(cat_path))
-        elif e_catfile == "cat":
-            print '\033[1;34mDetecting stars on %s!\033[0m' %(cat_path)
-            coordinate = os.popen("cat %s|tail -n +2|awk '{print $2,$3}'" %(cat_path)) 
-        
-        counter = 0
-        for line in coordinate.readlines():
-            counter += 1
-            line = line.replace("\n", "")
-            x_coor,  y_coor = line.split(" ")
-            d.set('regions', 'image; circle(%s,%s,5) # color=red text={%s}' %(x_coor,  y_coor, counter))
-        print '\033[1;34mDetecting stars job is completed!\033[0m'
+            return False
+            raise SystemExit
         return True
