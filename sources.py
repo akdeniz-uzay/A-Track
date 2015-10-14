b189346496c7fabed68d7bd3ef6a6c3b026ef18f
Tolga ATAY <tolga.phd@gmail.com>
2015-10-14 14:09:11 +0300

    Tolga Changes: - Minor PEP8 corrections.

--- sources.py
+++ sources.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Authors: Yücel Kılıç, Tolga Atay, Murat Kaplan, Nurdan Karapınar
+# Authors: Yücel Kılıç, Murat Kaplan, Nurdan Karapınar, Tolga Atay.
 # This is an open-source software licensed under GPLv3.
 
 
@@ -24,9 +24,12 @@ config = ConfigParser()
 
 if os.path.exists('./modpy.config'):
     config.read('./modpy.config')
+
 else:
-    print('Where is your modpy.config file?')
+    print('Python cannot open the configuration file. Make sure modpy.config',
+          'is in the same folder as mod.py.')
     raise SystemExit
+
 
 def align(fitsdir, reference, outdir):
 
@@ -56,22 +59,24 @@ def align(fitsdir, reference, outdir):
                                     makepng=False, verbose=False)
 
 
-def make_catalog(fitsdir, outdir, 
-                 DETECT_THRESH = float(config.get('sources', 'DETECT_THRESH')), 
-                 ANALYSIS_THRESH = float(config.get('sources', 'ANALYSIS_THRESH')),
-                 DETECT_MINAREA = float(config.get('sources', 'DETECT_MINAREA')), 
-                 PIXEL_SCALE = float(config.get('sources', 'PIXEL_SCALE')), 
-                 SEEING_FWHM = float(config.get('sources', 'SEEING_FWHM')),
-                 PHOT_AUTOPARAMS = config.get('sources', 'PHOT_AUTOPARAMS'),
-                 BACK_SIZE = int(config.get('sources', 'BACK_SIZE')),
-                 BACK_FILTERSIZE = int(config.get('sources', 'BACK_FILTERSIZE')), 
-                 DEBLEND_NTHRESH = int(config.get('sources', 'DEBLEND_NTHRESH')), 
-                 SATUR_LEVEL = float(config.get('sources', 'SATUR_LEVEL')),
-                 DEBLEND_MINCONT = float(config.get('sources', 'DEBLEND_MINCONT')), 
-                 GAIN = float(config.get('sources', 'GAIN')),
-                 rerun = config.get('sources', 'rerun'),
-                 keepcat = config.get('sources', 'keepcat'),
-                 verbose = config.get('sources', 'verbose')):
+def make_catalog(fitsdir, outdir,
+                 DETECT_THRESH=float(config.get('sources', 'DETECT_THRESH')),
+                 ANALYSIS_THRESH=float(config.get('sources',
+                                                  'ANALYSIS_THRESH')),
+                 DETECT_MINAREA=float(config.get('sources', 'DETECT_MINAREA')),
+                 PIXEL_SCALE=float(config.get('sources', 'PIXEL_SCALE')),
+                 SEEING_FWHM=float(config.get('sources', 'SEEING_FWHM')),
+                 PHOT_AUTOPARAMS=config.get('sources', 'PHOT_AUTOPARAMS'),
+                 BACK_SIZE=int(config.get('sources', 'BACK_SIZE')),
+                 BACK_FILTERSIZE=int(config.get('sources', 'BACK_FILTERSIZE')),
+                 DEBLEND_NTHRESH=int(config.get('sources', 'DEBLEND_NTHRESH')),
+                 SATUR_LEVEL=float(config.get('sources', 'SATUR_LEVEL')),
+                 DEBLEND_MINCONT=float(config.get('sources',
+                                                  'DEBLEND_MINCONT')),
+                 GAIN=float(config.get('sources', 'GAIN')),
+                 rerun=config.get('sources', 'rerun'),
+                 keepcat=config.get('sources', 'keepcat'),
+                 verbose=config.get('sources', 'verbose')):
 
     '''
     Creates SExtractor catalogs from FITS files.
@@ -80,30 +85,29 @@ def make_catalog(fitsdir, outdir,
     @type fitsdir: string
     @param outdir: Output directory for SExtractor's catalog files.
     @type outdir: string
-    @param DETECT_THRESH: Threshold value. If a single value is given, it
-    is interpreted as the threshold in units of the background standard
-    deviation.
+    @param DETECT_THRESH: Number of σ’s above the local background that an
+    object must have in order to be detected.
     @type DETECT_THRESH: float, integer
     @param ANALYSIS_THRESH: Surface brightness threshold at which CLASS STAR
-    and FWHM operate. 1 argument: relative to background RMS. 2 arguments: mu
+    and FWHM operate. 1 argument: Relative to background RMS. 2 arguments: mu
     (mag.arcsec^−2 ), Zero-point (mag).
     @type ANALYSIS_THRESH: float, integer
-    @param DETECT_MINAREA: Minimum number of pixels a group should have to
-    trigger detection. This parameter can be used just like DETECT_THRESH to
-    detect only the bright and “big” sources, or to increase detection
+    @param DETECT_MINAREA: Minimum number of pixels a group should have inorder
+    to trigger detection. This parameter can be used just like DETECT_THRESH to
+    detect only the bright and 'big' sources, or to increase detection
     reliability. However, it is tricky to manipulate at low detection
     thresholds because of the complex interplay of the object topology, noise
     correlations (including those induced by filtering), and sampling.
     Therefore, it is recommended to keep DETECT_MINAREA small, typically 1 to 5
     pixels and let DETECT_THRESH define SExtractor’s sensitivity.
     @type DETECT_MINAREA: float, integer
-    @param PIXEL_SCALE: Pixel size in arcsec for the surface brightness
-    parameters, FWHM, and star/galaxy separation.
+    @param PIXEL_SCALE: Pixel scale subtended by the telescope/CCD system
+    (arcsec).
     @type PIXEL_SCALE: float, integer
     @param SEEING_FWHM: FWHM of stellar images in arcsec for star/galaxy
     separation.
     @type SEEING_FWHM: float, integer
-    @param PHOT_AUTOPARAMS: MAG_AUTO parameters: <Kron_fact>,<min_radius>.
+    @param PHOT_AUTOPARAMS: MAG_AUTO parameters: <Kron_fact>, <min_radius>.
     @type PHOT_AUTOPARAMS: float, integer
     @param BACK_SIZE: Background mesh: <size> or <width>,<height>.
     @type BACK_SIZE: float, integer
@@ -111,11 +115,11 @@ def make_catalog(fitsdir, outdir,
     @type BACK_FILTERSIZE: float, integer
     @param DEBLEND_NTHRESH: Number of deblending sub-thresholds.
     @type DEBLEND_NTHRESH: float, integer
-    @param SATUR_LEVEL: Level in ADU at which arises saturation.
+    @param SATUR_LEVEL: Saturation level (ADU).
     @type SATUR_LEVEL: float, integer
     @param DEBLEND_MINCONT: Minimum contrast parameter for deblending.
     @type DEBLEND_MINCONT: float, integer
-    @param GAIN: Detector gain in e-/ADU.
+    @param GAIN: Detector gain (e-/ADU).
     @type GAIN: float, integer
     @param rerun: Runs SExtractor even if the catalog directory already exists.
     @type rerun: boolean
@@ -143,7 +147,7 @@ def make_catalog(fitsdir, outdir,
                              'BACK_SIZE': BACK_SIZE,
                              'BACK_FILTERSIZE': BACK_FILTERSIZE,
                              'FILTER': 'Y',
-                             'VERBOSE_TYPE': 'NORMAL' if verbose else 'QUIET'},
+                             'VERBOSE_TYPE': 'QUIET'},
                   params=['FLAGS', 'X_IMAGE', 'Y_IMAGE', 'FLUX_AUTO',
                           'BACKGROUND', 'FWHM_IMAGE', 'ELONGATION',
                           'FLUXERR_AUTO'],
