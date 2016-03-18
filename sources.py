6fd1145cc2c0a08cad28b47b3e043b808d192f42
Tolga ATAY <tolga.phd@gmail.com>
2016-03-18 09:36:43 +0200

    Removed unnecessary lines.

--- sources.py
+++ sources.py
@@ -47,8 +47,6 @@ def align(fitsdir, reference, outdir):
 
     if not reference:
         reference = images[0]
-    else:
-        reference = reference
 
     identifications = alipy.ident.run(reference, images, visu=False,
                                       sexkeepcat=False, verbose=False)
