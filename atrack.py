f74c1129e85f53a78f4cfd20b8c468a99e9def9b
Yücel KILIÇ <yucelkilic@myrafproject.org>
2017-07-11 15:19:32 +0300

    bug fix: FITS file check

--- atrack.py
+++ atrack.py
@@ -88,6 +88,10 @@ if __name__ == '__main__':
     arguments = parser.parse_args()
     fitsdir, reference = arguments.fits_dir, arguments.ref
 
+    if len(sorted(glob.glob(fitsdir + '/*.fit?'))) == 0:
+        print('No image FITS found in the {0}'.format(fitsdir))
+        raise SystemExit
+
     outdir = fitsdir + '/atrack'
 
     if not os.path.isdir(outdir):
